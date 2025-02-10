cf938a5516e6bfdbf30b58fb325afeaf1e26911e
amalnathsathyan <amalnathsathyan@gmail.com>
2025-02-10 13:01:55 +0530

    Adding anchor-bankrun tests

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -6,65 +6,52 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod votingdapp {
-    use super::*;
+  use super::*;
 
-  pub fn close(_ctx: Context<CloseVotingdapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdapp.count = ctx.accounts.votingdapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdapp.count = ctx.accounts.votingdapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeVotingdapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.votingdapp.count = value.clone();
+  pub fn initialize_poll(
+    ctx:Context<InitializePoll>,
+    poll_id:u64,
+    description:String,
+    poll_start:u64,
+    poll_end:u64,
+    poll_candidates: Vec<Pubkey>
+  ) -> Result<()>{
+    ctx.accounts.poll.set_inner(Poll{
+      poll_id,
+      description,
+      poll_start,
+      poll_end,
+      poll_candidates
+    });
     Ok(())
   }
 }
 
 #[derive(Accounts)]
-pub struct InitializeVotingdapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info>{
 
+  #[account(mut)]
+  pub admin: Signer<'info>,
   #[account(
-  init,
-  space = 8 + Votingdapp::INIT_SPACE,
-  payer = payer
+    init,
+    payer=admin,
+    space=8+ Poll::INIT_SPACE,
+    seeds= [b"Poll",poll_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub votingdapp: Account<'info, Votingdapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVotingdapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub votingdapp: Account<'info, Votingdapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub votingdapp: Account<'info, Votingdapp>,
+  pub poll: Account<'info,Poll>,
+  pub system_program: Program<'info, System>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Votingdapp {
-  count: u8,
+pub struct Poll {
+  pub poll_id:u64,
+  #[max_len(100)]
+  pub description: String,
+  pub poll_start:u64,
+  pub poll_end:u64,
+  #[max_len(5,32)]
+  pub poll_candidates: Vec<Pubkey>
 }
--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -1,76 +1,28 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
+import {Keypair, PublicKey, SystemProgram} from '@solana/web3.js'
 import {Votingdapp} from '../target/types/votingdapp'
+import { BankrunProvider} from "anchor-bankrun";
+import {startAnchor} from 'solana-bankrun';
+
+const IDL = require('../target/idl/votingdapp.json')
+const votingDappAddress = 'coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF');
 
 describe('votingdapp', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
 
-  const program = anchor.workspace.Votingdapp as Program<Votingdapp>
+  it('Initializes The Poll', async () => {
+    const context = await startAnchor("",[{name:"voting",programId:votingDappAddress}],[])
+    const provider = new BankrunProvider(context);
 
-  const votingdappKeypair = Keypair.generate()
+    const votingProgram = new Program<Votingdapp>(IDL,provider);
 
-  it('Initialize Votingdapp', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        votingdapp: votingdappKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([votingdappKeypair])
-      .rpc()
-
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(0)
-  })
-
-  it('Increment Votingdapp', async () => {
-    await program.methods.increment().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment Votingdapp Again', async () => {
-    await program.methods.increment().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Votingdapp', async () => {
-    await program.methods.decrement().accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set votingdapp value', async () => {
-    await program.methods.set(42).accounts({ votingdapp: votingdappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdapp.fetch(votingdappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the votingdapp account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        votingdapp: votingdappKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.votingdapp.fetchNullable(votingdappKeypair.publicKey)
-    expect(userAccount).toBeNull()
+    await votingProgram.methods.initializePoll(
+      new anchor.BN('1001'),
+      "Who should be the upgrade authority?",
+      new anchor.BN(''),
+      new anchor.BN(''),
+      [new PublicKey('8ihFoSeui5Dq1a46Zgj2o8F5DtjxWKMKjxm6jMsUZ3MK'),new PublicKey('JE8fYGocJkzrAiBsihmH3bycdb6kRHkDcud2oxRs9RH9')]
+    )
+  
   })
 })
